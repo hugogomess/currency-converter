8cb48da8d2f728fbf96fb7c60a5443f70db34332
hugogomess <hugogomes02@gmail.com>
2019-08-23 22:22:50 -0300

    add currency service

--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConverterComponent } from './components/converter/converter.component';
 
+import { ConverterComponent } from './components';
+import { CurrencyService } from './services';
 
 
 @NgModule({
   declarations: [ConverterComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ConverterComponent
+  ],
+  providers: [
+    CurrencyService
   ]
 })
 export class ConverterModule { }
