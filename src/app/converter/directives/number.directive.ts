6a2577af306e1491bdbc008740c85e486d143622
hugogomess <hugogomes02@gmail.com>
2019-08-27 10:25:19 -0300

    create number directive

--- src/app/converter/directives/number.directive.ts
+++ src/app/converter/directives/number.directive.ts
@@ -1,10 +1,47 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
 
 @Directive({
-  selector: '[appNumber]'
+  selector: '[appNumber]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumberDirective,
+    multi: true
+  }]
 })
-export class NumberDirective {
+export class NumberDirective implements ControlValueAccessor {
 
-  constructor() { }
+  onChange: any;
+  onTouched: any;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('keyup', ['$event'])
+  onKeyUp($event: any) {
+    let value = $event.target.value;
+    const decimalDotPosition = value.indexOf('.');
+
+    value = value.replace(/[\D]/g, '');
+
+    if (decimalDotPosition > 0) {
+      value = value.substr(0, decimalDotPosition) + '.' + value.substr(decimalDotPosition);
+    }
+
+    $event.target.value = value;
+    this.onChange(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value;
+  }
 
 }
